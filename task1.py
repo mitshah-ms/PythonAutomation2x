227b9114e61fe78fb3e0aba565628229032866b9
mitshah.ms <mitshah.ms@gmail.com>
2024-02-09 21:22:24 +0530

    task 1 and task 2 done

---
+++ task1.py
@@ -0,0 +1,14 @@
+#Task 1 - Take a user input (name, age, roll_number, phone_number) and print the user details.
+name = input ("Enter a name->")
+print (name)
+age = input ("Enter your age->")
+print (age)
+roll_number = input ("Enter roll number->")
+print (roll_number)
+phone_number = input ("phone_number->")
+print (phone_number)
+
+print ("________________")
+
+print ("name = ",name,"| age = ",age,"| roll_number = ",roll_number,"| phone_number = ",phone_number)
+
