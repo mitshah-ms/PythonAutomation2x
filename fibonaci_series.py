aa1fe297970529f80d2e70b75c3b04fc95e9b21c
mitshah.ms <mitshah.ms@gmail.com>
2024-02-11 19:34:43 +0530

    fibonaci correction of 9th feb from windows

--- fibonaci_series.py
+++ fibonaci_series.py
@@ -2,13 +2,14 @@
 
 fibonaci_lmit = int(input("Enter limit for fibonaci series -> "))  # 7
 
-fibo = 0
-print(fibo)
+ans = 0
+print(ans)
 n1 = 0
 n2 = 1
-
+# print(n1, end=" ")
+# print(n2, end=" ")
 for i in range(fibonaci_lmit):
-    ans = i + n2 #1 3
-    fibo = n2 + ans #2 5
-    print(f"ans = {ans}")
-    print((f"fibo = {fibo}"))
+    ans = n1 + n2
+    print(ans)
+    n1 = n2
+    n2 = ans
