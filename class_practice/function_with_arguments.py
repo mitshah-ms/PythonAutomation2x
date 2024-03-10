750c93615f9041b87acf80949a127ed4e0916a93
mit <mitshah.ms@gmail.com>
2024-03-10 18:55:40 +0530

    New function practice from Ubuntu

---
+++ class_practice/function_with_arguments.py
@@ -0,0 +1,18 @@
+def sum_of_num(a, b):
+    return a + b
+
+
+n1 = sum_of_num(10, 15)
+print(n1)
+print(n1 + n1)
+
+n2 = sum_of_num('mit', '\tshah')
+print(n2)
+print (f"{n2 + n2}")
+
+n4 = sum_of_num(n1, 25.50)
+print(n4)
+print(n1 + n4)
+
+n3 = sum_of_num(n2, n2)
+print(n3)
---
+++ class_practice/function_with_diff_arguments.py
@@ -0,0 +1,11 @@
+def say_hellO():
+    print("Hello")
+
+
+# say_hellO()
+
+for i in range(1, 10):
+    if i % 2 != 0:
+        say_hellO()
+        print(i)
+    pass
